41efbecd42f069562516b64515db0e3484475fa9
Arkadiusz Kulpa <arkadiusz.kulpa.91@gmail.com>
2024-04-05 08:56:36 +0100

    added calculate_results function

--- helper_functions.py
+++ helper_functions.py
@@ -4,7 +4,22 @@
 import tensorflow as tf
 
 
+# Function to evaluate: accuracy, precision, recall, f1-score
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 
+def calculate_results(y_true, y_pred):
+  """
+  Calcualtes model accuracy, precision, recall and f1 score of a binary classification model.
+  """
+  # Calculate model accuracy
+  model_accuracy = accuracy_score(y_true,y_pred) * 100
+  # Calcualte model precision, recall and f1-score using "weighted" average
+  model_precision, model_recall, model_f1, _ = precision_recall_fscore_support(y_true, y_pred, average="weighted")
+  model_results = {"accuracy": model_accuracy,
+                   "precision": model_precision,
+                   "recall": model_recall,
+                   "f1": model_f1}
+  return model_results
 
 def define_trainable_layers(base_model, fine_tune_layers_no, ifall=None):
   """
